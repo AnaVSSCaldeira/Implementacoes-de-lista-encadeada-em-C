4d98715fe318c83900bd6cf438cabbb8b6a87cc6
AnaValente <anavsscaldeira@gmail.com>
2021-06-03 16:55:30 -0300

    Alguns ajustes no código
    Nessa atualização coloquei alguns comentários explicando melhor o funcionamento do código e das funções.

--- codigos/bibliotecas/lista.h
+++ codigos/bibliotecas/lista.h
@@ -5,42 +5,44 @@ struct NO
     no *p; //ponteiro tipo no que aponta para o prox no
 };
 
-no *l=NULL;
+no *l=NULL; //lista tipo no como variavel global inicia como nula
 
-void desalocaValores(no *lista) //dar um free na lista
+//parametro: lista encadeada
+void desalocaValores(no *lista) //dar um free na lista ao final do programa
 {
-    if (lista->p != NULL)
+    if (lista->p != NULL) //enquanto nao estiver vazia
     {
-        desalocaValores(lista->p);
+        desalocaValores(lista->p); //chama a propria funcao
     }
     free(lista);
     return;
 }
 
-void escrevertxt(no *l){
+//parametro: lista encadeada
+void escrevertxt(no *l){ //escrever em um arquivo txt
     printf("\n\nESCREVENDO EM UM ARQUIVO TXT\n");
-    typedef struct ARQUIVO arquivo; //criando a estrutura no
+    typedef struct ARQUIVO arquivo; //criando a estrutura do arquivo
     struct ARQUIVO
     {
         int v; //valor do no
     };
 
     FILE *arq; //cria o arquivo
-    char arqaux;
+    char arqaux; //auxiliar para andar no arquivo
 
-    if ((arq = fopen("lista.txt", "w")) == NULL) //nao consegue abrir o arquivo
+    if ((arq = fopen("lista.txt", "w")) == NULL) //nao consegue abrir o arquivo lista.txt
         printf("Não foi possivel abrir o arquivo!");
     else //consegue abrir o arquivo pra escrever
     {
         arquivo umvalor;
         int sair = 0;
-        while (sair != -1)
+        while (sair != -1) //vai adicionando ate sair ser diferente de -1
         {
-        printf("\nInforme o numero que quer adicionar: ");
-        scanf("%d", &umvalor.v);
-        printf("Quer sair? Digite -1: ");
-        scanf("%d", &sair);
-        arqaux = fprintf(arq, "%d\n", umvalor.v);
+            printf("\nInforme o numero que quer adicionar: ");
+            scanf("%d", &umvalor.v);
+            printf("Quer sair? Digite -1: ");
+            scanf("%d", &sair);
+            arqaux = fprintf(arq, "%d\n", umvalor.v); //escreve o valor no arquivo
         }
 
         fclose(arq); //fecha o arquivo
@@ -54,6 +56,7 @@ void escrevertxt(no *l){
         printf("\n**********************\n");
         int numAux = 0;
         while (fscanf(arq, "%d", &numAux) != EOF){
+            //lê do arquivo, adiciona na lista e mostra na tela os valores adicionados
             no *aux = NULL; //aux pra andar na lista
             aux = (no *)malloc(sizeof(no));
             aux->v = numAux;
@@ -62,38 +65,15 @@ void escrevertxt(no *l){
             aux->p = lnum;
             lnum = aux;
         }
+        //ao final, desaloca a lista
         desalocaValores(lnum);
         }
     }
     fclose(arq);
 }
 
-void lertxt(no *l){
-    printf("\n\nLENDO EM UM ARQUIVO TXT\n");
-    FILE *arq;
-    if ((arq = fopen("lista.txt", "r")) == NULL) //nao consegue abrir o arquivo
-        printf("Não foi possivel abrir o arquivo!");
-        else //consegue abrir o arquivo para leitura
-        {
-        printf("\n**********************\n");
-        no *lnum = NULL;
-        int numAux = 0;
-        while (fscanf(arq, "%d", &numAux) != EOF){
-            no *aux = NULL; //aux pra andar na lista
-            aux = (no *)malloc(sizeof(no));
-            aux->v = numAux;
-
-            printf("\nNumero: %d", aux->v);
-            aux->p = lnum;
-            lnum = aux;
-        }
-        desalocaValores(lnum);
-        }
-    
-    fclose(arq);
-}
-
-int sizeL(no *l){
+//parametro: lista encadeada
+int sizeL(no *l){ //retorna o tamanho da lista
     if(l!=NULL){
         int cont=0;
         while(l!=NULL){
@@ -117,60 +97,73 @@ void impL(no *l){//IMPRIME LISTA
     printf("\n\n");
 }
 
-void scanL(int N, int P){
+//parametros: numero e posicao
+void scanL(int N, int P){ //recebe os elementos para adicionar na lista
     int T;
-    T=sizeL(l);
-    if(P>=0 && P<=T){
-        no *novo=malloc(sizeof(no));
-        novo->v=N;
-        novo->p=NULL;
+    T=sizeL(l);//pega o tamanho da lista encadeada
+    if(P>=0 && P<=T){//se a posicao for maior/igual a 0 e menor/igual que o tamanho
+        no *novo=malloc(sizeof(no));//aloca um espaco na memoria
+        novo->v=N;//no apontado valor é igual ao numero que a pessoa digitou
+        novo->p=NULL;//no apontado proximo = a NULL
+
+        //se a lista estiver vazia
         if(T==0){
-            l=novo;
+            l=novo; //lista recebe o novo no
         }
+        //se nao estiver vazia mas quer adicionar no inicio
         else if(P==0){
-            novo->p=l;
+            novo->p=l;//
             l=novo;
         }
+        //se for adicionar no meio ou no final
         else{
-            no *aux=l;
+            no *aux=l;//auxiliar para andar na lista
             int i;
-            for(i=0;i<P-1;i++){
+            for(i=0;i<P-1;i++){ //for para andar ate a posicao que a pessoa quer (-1)
                 aux=aux->p;
             }
+            //novo apontado proximo recebe o aux apontado proximo
             novo->p=aux->p;
             //1 -> 4 -> 2 3 -> 5 -> NULL
             aux->p=novo;
         }
+    //se a posicao nao for valida
     }else{
         printf("\nPosicao invalida!\n\n");
     }
 }
 
-int findnum(no *l, int N, int S){ //lista, numero, tamanho da lista
-    int F=-2, i;
+//parametros: lista, numero, tamanho da lista
+int findnum(no *l, int N, int S){ //acha um numero da lista
+    int F=-2, i; //flag para dizer se achou ou lista esta vazia
+    //lista nao esta vazia
     if(l!=NULL){
-        no *aux=l;
-        for(i=0;i<S;i++){
-            if(aux->v==N){
+        no *aux=l;//criar um auxiliar para perdorrer a lista
+        for(i=0;i<S;i++){//percorre em relacao ao tamanho
+            if(aux->v==N){ //se achar o numero, guarda a posicao
                 F=i;
-                return F;
+                return F; //retorna a posicao pelo flag
             }
             aux=aux->p;
         }
+        //se nao achar, retorna -2
         if(F==-2){
             return F;
         }
     }
+    //lista vazia, retorna -1
     else{
         F=-1;
         return F;
     }
 }
 
-int findpos(no *L, int P){
+//parametros: lista encadeada, posicao
+int findpos(no *L, int P){ //mostra o numero pela posicao
     no *aux=L;
-    int i=0,valor=-1, T;
+    int i=0,valor=-1, T; //valor = -1 se nao ahcar pela posicao e tamanho da lista
     T=sizeL(l);
+    //se a lsita nao estiver vazia e a posicao for correspondente
     if(L!=NULL && (P>=0 && P<T)){
         while(aux!=NULL){
             if(P==i){
@@ -180,10 +173,12 @@ int findpos(no *L, int P){
             i++;
         }
     }
+    //se a lista nao estiver vazia mas a posicao for invalida
     else if(L!=NULL){
         printf("\nPosicao invalida\n");
         return valor;
     }
+    //se estiver vazia
     else{
         printf("\nLista vazia\n");
         return valor;
@@ -191,7 +186,8 @@ int findpos(no *L, int P){
     return valor;
 }
 
-void ordenaL(no *L, int S){
+//parametros: lista encadeada e tamanho da lista
+void ordenaL(no *L, int S){ //ordena pelo metodo Bubble Sort
     if(L!=NULL){
         int cont=1, i, recebe;
         no *aux;
@@ -215,30 +211,40 @@ void ordenaL(no *L, int S){
     }
 }
 
+//parametro: numero para remover
 void remL(int N){
     int T,f=0;
-    T=sizeL(l);
+    T=sizeL(l); //pega o tamanho da lista
+    //se a lista nao estiver vazia
     if(T>0){
+        //se a lista tiver um so elemento e ele for o que o usuario digitou
         if(T==1 && l->v==N){
             l=NULL;
         }
+        //se a lista tiver mais de um elemento e ele for o primeiro da lista
         else if(T>1 && l->v==N){
             l=l->p;
         }
+        //se o elemto estiver no meio ou no final da lista
         else{
-            f=findnum(l,N,T);
+            f=findnum(l,N,T);//tenta encontrar o numero
+            //se encontrar
             if(f!=-2){
                 no *aux=l;
+                //percorre ate o valor do proximo aux for N ou quando o proximo for NULL
                 while(aux->p->v!=N || aux->p==NULL){
                     aux=aux->p;
                 }
+                //o proximo do aux aponta para o no depois do proximo dele
                 aux->p=aux->p->p;
                 printf("\nNumero removido!\n\n");
             }
+            //se nao achar o numero
             else{
                 printf("\nNumero nao encontrado!\n\n");
             }
         }
+    //se a lista estiver vazia
     }else{
         printf("\nLista vazia!\n\n");
     }
--- codigos/codigo em c/listaenc.c
+++ codigos/codigo em c/listaenc.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lista.h"
+#include "lista.h" //biblioteca que criei
 
 int main(void)
 {
     int op=100, n, size=0, find, pos;
-
+    /*opcao para ser escolhida, numero para inserir/remover, tamanho da lista, variavel
+    para achar um elemento, e posicao*/
     while(op!=11){
+        //lista de opcoes
         printf("\n(1)Inserir elemento\n(2)Mostrar lista\n(3)Remover elemento\n(4)Mostrar tamanho da lista\n(5)Buscar elemento\n(6)Mostrar elemento por posicao\n(7)Escrever em um arquivo TXT\n(8)Ordenar lista\n(9)Sair\n\n");
         scanf("%d",&op);
-        if(op==1){
+        if(op==1){ //para adicionar
             printf("Digite o numero que deseja adicionar: ");
             scanf("%d",&n);
             printf("Agora, digite a posicao para adiciona-lo: ");
             scanf("%d",&pos);
             scanL(n,pos);
         }
-        else if(op==2){
+        else if(op==2){ //para mostrar a lista
             impL(l);
         }
-        else if(op==3){
+        else if(op==3){ //para remover algum elemento da lista
             printf("Digite o numero que deseja remover: ");
             scanf("%d",&n);
             remL(n);
         }
-        else if(op==4){
+        else if(op==4){ //para saber o tamanho da lista
             printf("\nA lista possui %d elemento(s)\n\n",sizeL(l));
         }
-        else if(op==5){
+        else if(op==5){ //para procurar um elemento na lista
             printf("Digite o numero que deseja procurar na lista: ");
             scanf("%d",&n);
             size=sizeL(l);
@@ -42,27 +44,27 @@ int main(void)
                 printf("\n%d se encontra na posicao %d\n\n",n,find);
             }
         }
-        else if(op==6){
+        else if(op==6){ //procurar elemento por posicao
             printf("\nDigite a posicao para mostrar o numero correspondente: ");
             scanf("%d",&pos);
             n=findpos(l,pos);
             printf("\nNumero encontrado: %d\n\n",n);
         }
-        else if(op==7){
+        else if(op==7){ //escrever em um arquivo
             printf("\nEscrevendo no arquivo:\n\n");
             escrevertxt(l);
             printf("\n\n");
         }
-        else if(op==8){
+        else if(op==8){ //ordenar a lista por bubblesort
             printf("\nLista ordenada:");
             size=sizeL(l);
             ordenaL(l,size);
         }
-        else if(op==9){
+        else if(op==9){ //finalizando o programa e desalocando a lista
             printf("\nFinalizando...");
             desalocaValores(l);
         }
-        else{
+        else{ //se a pessoa digitar uma opcao invalida
             printf("\nOpcao invalida...\n");
         }
     }
